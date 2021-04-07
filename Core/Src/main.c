9d85005415ea21b51819c787013bfecc87cd9f9a
Zhao_ch <Zhao_ch@163.com>
2021-04-07 13:08:27 +0800

    System ChipID read ok

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -2,6 +2,8 @@
 
 uint32_t main_loop = 0;
 
+ChipIDTypeDef SystemChipID;
+
 /**
   * @brief  The application entry point.
   * @retval int
@@ -17,6 +19,10 @@ int main(void)
     MX_GPIO_Init();
     MX_DMA_Init();
     MX_USART1_UART_Init();
+
+    GET_ChipID(&SystemChipID);
+    printf("System chipID is: %lx%lx%lx\r\n", SystemChipID.Chip_ID_H,
+            SystemChipID.Chip_ID_M, SystemChipID.Chip_ID_L);
 
     while (1) {
         printf("System main_loop is: %ld\r\n", main_loop);
